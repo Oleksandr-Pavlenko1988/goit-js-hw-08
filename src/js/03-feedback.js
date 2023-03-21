83cdb4c7b55f6934c15b609eb60a520e0c1983f4
Oleksandr <elvenmonuskrypt@gmail.com>
2023-03-21 22:53:30 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(handleFormInput, 500));
+form.addEventListener('submit', handleFormSubmit);
+
+const STORAGE_KEY = "feedback-form-state"
+
+initForm();
+
+function handleFormInput(e) {
+    let inputData = localStorage.getItem(STORAGE_KEY);
+    inputData = inputData ? JSON.parse(inputData) : {};
+    inputData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(inputData));
+}
+
+function initForm() {
+    let savedInput = localStorage.getItem(STORAGE_KEY);
+    if(savedInput) {
+        savedInput = JSON.parse(savedInput);
+        Object.entries(savedInput).forEach(([name, value]) => {
+            form.elements[name].value = value;
+        });
+    }
+}
+function handleFormSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    formData.forEach((name, value) => console.log(name, value));
+    localStorage.removeItem(STORAGE_KEY);
+    e.currentTarget.reset();
+}
